3f6cc5f780193c98e7dac8de40035bea186cced0
Fizza-Khalid41 <fizzakhalid982@gmail.com>
2025-10-26 16:35:45 +0500

    studybud-ninth commit

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from .models import User, Room
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["name", "username", "email", "password1", "password2"]
+
 
 class RoomForm(ModelForm):
     class Meta:
@@ -8,7 +16,8 @@ class RoomForm(ModelForm):
         fields = "__all__"
         exclude = ["host", "participants"]
 
+
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["avatar", "name", "username", "email", "bio"]
--- studybud/settings.py
+++ studybud/settings.py
@@ -38,11 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "base.apps.BaseConfig"
+    "base.apps.BaseConfig",
+
+    'rest_framework',
+     "corsheaders",
 ]
+
+AUTH_USER_MODEL = "base.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,14 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'base/static')
+    os.path.join(BASE_DIR, 'base/static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 #STATIC_ROOT =
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
